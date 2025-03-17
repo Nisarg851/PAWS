a98be86005c992bcf237ba4131c19d5a46ae6ce7
Nisarg Mahyavanshi <nisargmahyavanshi8501@gmail.com>
2025-03-17 14:14:22 -0400

    AuthPage UI integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
+import AuthPage from "./Pages/AuthPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/auth" element={<AuthPage />} />
     </Routes>
   )
 }
