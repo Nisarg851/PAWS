af71e82b67ea1341be74e8d51413c9a98b3f6fed
Nisarg Mahyavanshi <nisargmahyavanshi8501@gmail.com>
2025-03-17 15:07:46 -0400

    CreateReportPage UI integrated

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from "./Pages/LandingPage";
 import AuthPage from "./Pages/AuthPage";
 import BrowsePage from "./Pages/BrowsePage";
 import ReportDetailPage from "./Pages/ReportDetailPage";
+import CreateReportPage from "./Pages/CreateReportPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/browse" element={<BrowsePage />} />
       <Route path="/report/:id" element={<ReportDetailPage />} />
+      <Route path="/create" element={<CreateReportPage />} />
     </Routes>
   )
 }
